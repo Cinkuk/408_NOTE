b9d543cd30ab350643d7a11a4553f7b85cce1556
Cinkuk <cjymail163@163.com>
2025-10-28 18:01:29 +0800

    update chap 5

---
+++ Chap5 Tree/Cpp/BTree.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+typedef int type;
+
+typedef struct BiTNode {
+    type data;                       // 数据域
+    struct BiTNode *lchild, *rchild; // 左右孩子指针
+} BiTNode, *BiTree;
+
+int main()
+{
+
+    return 0;
+}
